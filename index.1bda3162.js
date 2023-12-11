7ddff48efe75bfb8d2206818edff90fd697e0b7e
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-11 16:56:31 +0200

    make build

---
+++ index.1bda3162.js
@@ -0,0 +1,4 @@
+var e=document.querySelectorAll(".population"),t=document.querySelector(".total-population"),n=document.querySelector(".average-population"),o=0;e.forEach(function(e){var t=e.textContent.split(",").join("");o+=+t});var r=o/e.length;function a(e){return e.toLocaleString("en-US")}t.insertAdjacentText("beforeend",a(o)),n.insertAdjacentText("beforeend",a(r));// write your code here
+//# sourceMappingURL=index.1bda3162.js.map
+
+//# sourceMappingURL=index.1bda3162.js.map
